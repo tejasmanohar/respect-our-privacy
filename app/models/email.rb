79c8513c809fe2aa962f16b8b520ee4b18278846
Tejas Manohar <me@tejas.io>
2015-06-19 17:15:52 -0500

    greater than

--- app/models/email.rb
+++ app/models/email.rb
@@ -1,6 +1,6 @@
 class Email < ActiveRecord::Base
 
   validates :email, :uniqueness => true, :email => true, :presence => true
-  validates :requests_count, :numericality => { :less_than_or_equal_to => 5, only_integer: true }, presence: true
+  validates :requests_count, :numericality => { :greater_than => 0, :less_than_or_equal_to => 5, only_integer: true }, presence: true
 
 end
