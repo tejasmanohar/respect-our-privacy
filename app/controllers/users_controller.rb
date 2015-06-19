9012e4cb913fee1a78d95627c535049575beecaf
Tejas Manohar <me@tejas.io>
2015-06-19 16:36:11 -0500

    root routes, email + phone

---
+++ app/controllers/users_controller.rb
@@ -0,0 +1,15 @@
+class UsersController < ApplicationController
+
+    def new_phone
+        respond_to do |format|
+            format.html
+        end
+    end
+
+    def new_email
+      respond_to do |format|
+          format.html
+      end
+    end
+
+end
