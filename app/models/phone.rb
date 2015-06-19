ba1f1b1e020c56146ee3f04081153085edce356f
Tejas Manohar <me@tejas.io>
2015-06-19 17:51:21 -0500

    remove p

--- app/models/phone.rb
+++ app/models/phone.rb
@@ -4,7 +4,6 @@ class Phone < ActiveRecord::Base
   validates :requests_count, :numericality => { :less_than_or_equal_to => 5, only_integer: true }, presence: true
 
   def call
-    p ENV
     @client = Twilio::REST::Client.new ENV['TWILIO_ACCOUNT_SID'], ENV['TWILIO_AUTH_TOKEN']
     @call = @client.calls.create(
       from: ENV['TWILIO_NUMBER'],
