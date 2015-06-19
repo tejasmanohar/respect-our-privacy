c06279cbb40121f8560359cc04d4c55fa54def4a
Tejas Manohar <me@tejas.io>
2015-06-19 16:43:36 -0500

    user model, phone number + requests

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,15 +1,18 @@
 class UsersController < ApplicationController
 
-    def new_phone
-        respond_to do |format|
-            format.html
-        end
+  def new_phone
+    respond_to do |format|
+      format.html
     end
+  end
 
-    def new_email
-      respond_to do |format|
-          format.html
-      end
+  def new_email
+    respond_to do |format|
+      format.html
     end
+  end
+
+  def create
+  end
 
 end
